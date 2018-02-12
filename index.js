873293f3460d76a7049d2b22a7a02cb46bba0d68
Mietola Karoliina A <kmietola@da3-cs-dk110-16.pc.helsinki.fi>
2018-02-12 20:18:22 +0200

    muokattu

--- index.js
+++ index.js
@@ -2,107 +2,34 @@ const express=require('express')
 const app=express()
 const bodyParser=require('body-parser')
 const cors=require('cors')
-const Note=require('./models/note')
+const mongoose=require('mongoose')
+const middleware=require('/utils/middleware')
+const notesRouter=require('/controllers/notes')
 
-app.use(cors())
-app.use(express.static('build'))
-
-
-
-
-const formatNote =(note)=> {
-  return{
-    content: note.content,
-    date: note.date,
-    important: note.important,
-    id: note._id
-  }
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-  Note
-    .find({})
-    .then(notes => {
-      response.json(notes.map(formatNote))
-    })
-})
-
-app.use(bodyParser.json())
-
-app.get('/api/notes/:id', (request, response) => {
-  Note
-    .findById(request.params.id)
-    .then(note => {
-      if(note)  {
-        response.json(formatNote(note))
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({error: 'malformatted id'})
-    })
-})
-const generateId=()=> {
-  const maxId=notes.length > 0 ? notes.map(n => n.id).sort().reverse()[0] : 1
-  return maxId + 1
-}
-app.post('/api/notes', (request, response)=> {
-  const body=request.body
-
-  if(body.content===undefined) {
-    return response.status(400).json({error: 'content missing'})
-  }
-
-  const note= new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date()
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then( () => {
+    console.log('connected to database', process.env.MONGODB_URI)
+  })
+  .catch(err => {
+    console.log(err)
   })
 
-  note
-    .save()
-    .then(formatNote)
-    .then(formattedNote => {
-      response.json(formattedNote)
-    })
+mongoose.Promise=global.Promise
 
-})
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+app.use(middleware.logger)
 
-app.delete('/api/notes/:id', (request, response) => {
-  Note
-    .findByAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => {
-      response.status(400).send({error: 'malformatted id'})
-    })
-})
+app.use('/api/notes', notesRouter)
 
-app.put('/api/notes/:id', (request, response) => {
-  const body=request.body
-  const note={
-    content: body.content,
-    important: body.important
-  }
-  Note
-    .findByIdAndUpdate(request.params.id, note, {new: true} )
-    .then(updatedNote => {
-      response.json(formatNote(updatedNote))
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({error: 'malformatted id'})
-    })
-})
+app.use(middleware.error)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- models/note.js
+++ models/note.js
@@ -1,16 +1,5 @@
 const mongoose=require('mongoose')
 
-<<<<<<< HEAD
-const url= 'mongodb://fullstack:salis1@ds229448.mlab.com:29448/backend_database'
-=======
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-const url= process.env.MONGODB_URI
->>>>>>> 998c73259bf6272ba5a268cbac5224a9640e3ddb
-
-mongoose.connect(url)
 
 const Note=mongoose.model('Note', {
   content: String,
