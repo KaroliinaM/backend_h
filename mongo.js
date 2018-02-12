b22ea3b19b922bc4c239321d99ffa42c59cdc4d5
Your Name <karoliina.mietola@helsinki.fi>
2018-02-12 02:40:56 +0200

    muutoksia

--- mongo.js
+++ mongo.js
@@ -1,13 +1,23 @@
 const mongoose=require('mongoose')
-const url= 'mongodb://fullstack:<salasana>@ds229448.mlab.com:29448/backend_database'
+const url= 'mongodb://fullstack:salis1@ds229448.mlab.com:29448/backend_database'
 
 mongoose.connect(url)
-
-const Note = mongoose.model('Note', {
+const Schema=mongoose.Schema
+const noteSchema = new Schema({
   content: String,
   date: Date,
   important: Boolean
 })
+noteSchema.statics.findNote=function(note) {
+  return{
+    id: note._id,
+    content: note.content,
+    date: note.date,
+    important: note.important
+  }
+}
+const Note = mongoose.model('Note', noteSchema )
+
 
 Note
   .find({})
@@ -17,6 +27,16 @@ Note
     })
     mongoose.connection.close()
   })
+
+  Note
+    .find({})
+    .then(result => {
+      result.forEach(note => {
+        console.log(Note.findNote(note))
+      })
+      mongoose.connection.close()
+    })
+
 
 /**const note = new Note({
   content: 'HTML on helppoa',
