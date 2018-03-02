8b67ddc6423890a9fefacf37916077351edc2b8b
Your Name <karoliina.mietola@helsinki.fi>
2018-03-03 00:29:20 +0200

    muutoksia

--- controllers/login.js
+++ controllers/login.js
@@ -7,10 +7,10 @@ loginRouter.post('/', async(request, response)=> {
   const body=request.body
 
   const user=await User.findOne({username: body.username})
-  const passwordCorrect=user===null ?
+  console.log(user.passwordHash)
+  const passwordCorrect = user === null ?
     false :
     await bcrypt.compare(body.password, user.passwordHash)
-
   if (!(user && passwordCorrect) ) {
     return response.status(401).send({error: 'invalid username or password'})
   }
